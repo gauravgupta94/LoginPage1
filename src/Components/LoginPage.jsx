fd633ee4c21e405c905a0b6f7df9d812a6c5d1d1
Gaurav Gupta <gauravthecoolboy24@gmail.com>
2025-03-05 09:08:04 +0530

    background design (sort some css)

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -6,13 +6,13 @@ function LoginPage() {
     return (
         <div className='body grid place-items-center m-0 h-screen bg-[#e4d1f8] text-[#2e2c2f]'>
 
-            <img  src={Blob} alt="Background" className='blob fixed z-10 w-[85vw] max-w-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'/>
+            <img  src={Blob} alt="Background" className='blob fixed z-10 w-[150vw] max-w-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 before:absolute before:w-6 before:h-6 before:'/>
 
-            <div className='orbit fixed z-10 w-[95vw] max-w-[720px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 before:'></div>
+            <div className='orbit fixed z-10 w-[95vw] max-w-[720px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '></div>
 
             <div className='login flex flex-col justify-center items-center text-center relative z-20 bg-white width-[340px] pt-16 pr-8 pb-12 rounded-3xl shadow-lg'>
                 
-                <img src={Logo} alt="" className='w-20 m-0 mb-8'/>
+                <img src={Logo} alt="" className='w-20 '/>
                 <h2 className=''>Welcome to Scoobyy Bank</h2>
                 <h3 >Keep your money safe with us</h3>
                 <form action="" className='form'>
